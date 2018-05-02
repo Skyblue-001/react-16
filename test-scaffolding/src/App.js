3e5f89223310f65483a55c357a98dc23a72ed9ec
Teddy Ghim <teddyghim@gmail.com>
2018-05-02 18:14:28 +0200

    #feature 3 - Error Boundaries

--- test-scaffolding/src/App.js
+++ test-scaffolding/src/App.js
@@ -1,13 +1,30 @@
 import React, { Component, Fragment } from 'react';
 import { createPortal } from "react-dom";
 
-const Message = () => "Just touched it!";
+class ErrorMaker extends Component{
+  state = {
+    friends: ["jisu","flynn","daal","kneeprayer"]
+  }
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: 1
+      });
+    }, 2000);
+  };
+  render(){
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
 
 class Portals extends Component{
   render(){
     return createPortal(<Message />, document.getElementById("touchMe"));
   }
 }
+
+const Message = () => "Just touched it!";
 
 class FragmentReturn extends Component{
   render(){
@@ -27,13 +44,21 @@ class StringReturnType extends Component{
   }
 }
 
+const ErrorFallback = () => "Sorry sommething went wrong";
+
 class App extends Component {
+  componentDidCatch = (error, info) => {
+    console.log(`catched ${error} the info I have is ${JSON.stringify(info)}`);
+  };
+
   render() {
+    const { hassError } = this.state;
     return (
       <Fragment>
         <FragmentReturn />
         <StringReturnType />
         <Portals />
+        {hasError ? <ErrorFallback /> : <ErrorMaker />}
       </Fragment>
     );
   }
